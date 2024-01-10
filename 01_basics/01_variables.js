703a827673ddf1d366a608dcf498f0c841b4597b
dropthedoctor9 <153898435+dropthedoctor9@users.noreply.github.com>
2024-01-10 15:22:20 +0000

    var, let and const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId= 144553;
+let accountEmail= "hitesh@gooogle.co.in";
+var accountPassword= "1234567";
+accountCity= "Jaipur"; // not a good programming practice to declare identifiers/variables automatically also known as automatic declaration
+
+accountEmail="hc@hc.com"
+accountPassword="2121212231"
+accountCity="Bengaluru"
+let accountState; 
+
+// accountId= 2; // assignment or change is not allowed to a constant identifier/variable
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+/*
+ prefer not to use var because of issues in block and functional scope 
+*/
+
+/* 
+the above is an example of a multi line comment 
+*/
