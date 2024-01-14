6fc3b781053c2cf6593e753b1ee9aff102a0208f
dropthedoctor9 <153898435+dropthedoctor9@users.noreply.github.com>
2024-01-14 18:25:12 +0000

    arrays

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,16 +2,16 @@ const firstName= "Hitesh" // behind the scenes this also invokes an object
 const repoCount= 50
 
 // console.log(firstName+ repoCount+ " Value"); // this is an old way of writing strings since to concatinate we use plus and comma and also write raw strings using ""
-// in  modern times, to write strings we use backticks, and by using backticks we have string interpolation which gives us placeholders and we can inject variables(strings, numbers, etc) where-ever we can. we can also use methods here which is an advantage
+// in  modern times, to write strings we use backticks, and by using backticks we have string interpolation which gives us placeholders and we can inject variables(strings, numbers, etc) where-ever we can. important => we can also use methods in placeholders which is an advantage
 
 console.log(`Hello my name is ${firstName[0]} and my repo count is ${repoCount}`)// we can access key value pair here also 
 
-const gameName= new String("poulentspacegiggle") // this is another way of declaring strings, in this case we use a new keyword and hence we use objects in JavaScript and inside the construtor we write its name/ initialise
+const gameName= new String("poulentspacegiggle") // this is another way of declaring strings, in this case we use a new keyword and hence we use objects in JavaScript and inside the construtor we write its name/ initialise the string
 //when we write like this on console in a browser, we get key value pairs of the string, but string is an object here and not an array and we also get the prototype and the length property
 console.log(gameName)
 console.log(gameName[0]) // we are accessing its keys
 
-console.log(gameName.__proto__) // this is just a syntax to access prototype, here the result will come empty object/method but in console we will get all methods
+console.log(gameName.__proto__) // this is just a syntax to access prototype, here in Node the result will come empty object/method but in console we will get all methods
 // inside prototype we have methods/functions
 
 console.log(firstName.length) // length property
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,51 @@
+// () => paranthesis
+// [] => brackets
+// {} => braces
+
+// array is always an object
+// it is a single variable which stores multiple values which may or may not be of the same data type
+
+// arrays in javascript is resizable 
+// an array inside another array is also possible 
+// arrays have zero indexing that means first element always is at 0th position and so on 
+// arrays cannot be accessed using arbitary elements like myArray["one"] => called not associative in nature (see the meaning of associative array here to get things more clear)
+
+// in javascript while performing array copy operations it creates something known as SHALLOW COPIES and not DEEP COPIES
+
+// SHALLOW COPIES => shallow copy of an object is a copy whose properties share the same reference point as the source object as well => when you make changes to one copy the other copy will alsonbe affected as well ( refer the stack memory and heap memory video for better refernce)
+
+const myArray= [0,1,2,3,4,5] // here 0,1,2,3,4,5 => called elements
+// can be of any data type be it strings, numbers or even mixed data type including boolean values
+//console.log(myArray)
+
+// another way of declaring arrays in shown below
+const myArray2= new Array (1,2,3,4,5) // no square brackets simply pass the values in paranthesis 
+//console.log(myArray2) // does not show results like string in thid case tho 
+
+// ARRAY METHODS
+
+myArray.push(6,7,8) // to add elements to the array end of the array unlike unshift which adds to the beginning of the array
+//console.log(myArray)
+
+myArray.pop() // no arguements passed, pop only removes the last element of an array
+//console.log(myArray) 
+
+myArray.unshift(100)// prefixes an element to the beginning of the array which is not ideal because it will shift the indexes of all other elements as well which will put a lot of load on the system
+// console.log(myArray) 
+
+myArray.shift() // no atguement is passed => removes the first element of the array contray to the poop() which removed the last element of the array
+
+myArray.indexOf(55) // both in strings and array whenever the value doesn't exist the output is -1
+
+const hahaArray= myArray // copying one array to another 
+// console.log(hahaArray) // the array will be the same as the source array
+
+const hahaArray2 = myArray.join() // here it copies the array but it changes it data type into string
+//console.log(typeof(hahaArray2)) // the output is string
+
+myTodoArray = [10,11,12,13,14,15,16,17,18,19,20]
+console.log(myTodoArray.slice(0,4)) // returns the part of the array whose index have been given  and DOES NOT CHANGE THE ORIGINAL ARRAY
+console.log(myTodoArray)
+
+console.log(myTodoArray.splice(0,4)) // returns the part of an array whose index have been given and CHANGES THE ORIGINAL ARRAY
+console.log(myTodoArray)
